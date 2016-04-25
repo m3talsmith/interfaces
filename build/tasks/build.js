ff99652cdb0742a9ebff7e551428bdf093291f05
Michael Christenson II <michael@rebelhold.com>
2016-04-24 19:07:16 -0700

    added skeleton build

---
+++ build/tasks/build.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let gulp       = require("gulp")
+let sourceMaps = require("gulp-sourcemaps")
+let babel      = require("gulp-babel")
+
+gulp.task("build", () => {
+  return gulp.src("src/**/*.js")
+    .pipe(sourceMaps.init())
+    .pipe(babel())
+    .pipe(sourceMaps.write("."))
+    .pipe(gulp.dest("dist"))
+});
